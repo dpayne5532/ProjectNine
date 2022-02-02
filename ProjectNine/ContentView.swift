c17a6fe02e8716b031afe80420f8e575b782ce39
Dan Payne <dpayne5532@gmail.com>
2022-02-02 16:47:05 -0700

    Initial Commit

---
+++ ProjectNine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectNine
+//
+//  Created by Dan Payne on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectNine/ProjectNineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectNineApp.swift
+//  ProjectNine
+//
+//  Created by Dan Payne on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectNineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
